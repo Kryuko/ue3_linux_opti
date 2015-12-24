ff0b10a63fcc1e37a5c8247a10d7891d1190e425
Kryuko <gian95torino@hotmail.it>
2015-12-24 18:26:52 +0100

    Update MainEng.sh

--- MainEng.sh
+++ MainEng.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 MEM=$(grep -o "Memory:\ [0-9]*" /var/log/Xorg.0.log | grep -Eo "[0-9]+")
-#MEMORY=$(grep -o ".*Memory: .*" /var/log/Xorg.0.log | cut -d \  -f 9)
+
 let MEM=$MEM/1024
 echo $MEM
 
-zenity --width=600 --height=400 --info --text "By pressing the OK button you declare to accept our license. \n 
-Developed by:\n Gianmaria Generoso, admin of www.italiaunix.com.\n Alberto Pau - DebInst creator https://github.com/TheGatorade/debinst \n
-GNU GPL v3 License - Available at \n https://raw.githubusercontent.com/Kryuko/ue3_linux_opti/master/LICENSE"
+zenity --width=600 --height=400 --info \
        --title="License" \
-	#--text="Inserisci la tua memoria video in MB" \
-       #--checkbox="I read and accept the terms."
+	--text "By pressing the OK button you declare to accept our license. \n 
+Developed by:\n Gianmaria Generoso, admin of www.italiaunix.com.\n Alberto Pau - also debinst creator https://github.com/TheGatorade/debinst \n
+GNU GPL v3 License - Available at \n https://raw.githubusercontent.com/Kryuko/ue3_linux_opti/master/LICENSE"
 
 let MEMORY=$MEM-128
 
 zenity --width=600 --height=400 --info \
 --title="GPU Memory" \
---text="Your GPU memory is $MEM\MB, but it will be set to $MEMORY\MB to avoid problems." \
+--text="Your detected GPU memory is $MEM\MB, but it will be set to $MEMORY\MB to avoid issues." \
 echo $MEMORY 
 
 find ~/.local/share/ -type f -iname *.ini -exec sed -inr "s/^PoolSize=.*/PoolSize=$MEMORY/g" {} \;
 
-zenity --width=600 --height=400 --info --text "All your UE3 Games are fully optimized to $MEMORY MB"
+zenity --width=600 --height=400 --info --text "All your UE3 Games are optimized for $MEMORY\MB GPU."
 HALFMEMORY=$(($MEM / 4))
 echo $HALFMEMORY
 
