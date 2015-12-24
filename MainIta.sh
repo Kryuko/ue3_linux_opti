a193dbec55fff48e1ec416c85ec0326bbaf7050c
Kryuko <gian95torino@hotmail.it>
2015-12-24 18:12:23 +0100

    Update MainIta.sh

--- MainIta.sh
+++ MainIta.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-# My first script
 
 MEM=$(grep -o "Memory:\ [0-9]*" /var/log/Xorg.0.log | grep -Eo "[0-9]+")
 #MEMORY=$(grep -o ".*Memory: .*" /var/log/Xorg.0.log | cut -d \  -f 9)
 let MEM=$MEM/1024
 echo $MEM
 
-zenity --info --text "PREMENDO OK ACCETTI LA NOSTRA LICENZA \n 
-Programma creato da Gianmaria Generoso, admin di www.italiaunix.com, e Alberto Pau."
-       --title="Licenza" \
+zenity --width=600 --height=400 --info --text "PREMENDO OK ACCETTI LA NOSTRA LICENZA \n 
+Developed by:\n Gianmaria Generoso, admin of www.italiaunix.com.\n Alberto Pau - DebInst creator https://github.com/TheGatorade/debinst \n
+GNU GPL v3 License - Available at \n https://raw.githubusercontent.com/Kryuko/ue3_linux_opti/master/LICENSE"
+       --title="License" \
 	#--text="Inserisci la tua memoria video in MB" \
        #--checkbox="I read and accept the terms."
 
 let MEMORY=$MEM-128
 
-zenity --info \
+zenity --width=600 --height=400 --info \
 --title="Memoria video" \
 --text="La tua memoria video è di $MEM\MB, verrà impostata a $MEMORY\MB per evitare problemi." \
 echo $MEMORY 
 
 find ~/.local/share/ -type f -iname *.ini -exec sed -inr "s/^PoolSize=.*/PoolSize=$MEMORY/g" {} \;
 
-zenity --info --text "Tutti i tuoi giochi UE3 sono stati impostati a $MEMORY MB"
+zenity --width=600 --height=400 --info --text "Tutti i tuoi giochi UE3 sono stati impostati a $MEMORY MB"
 HALFMEMORY=$(($MEM / 4))
 echo $HALFMEMORY
 
 find ~/.local/share/ -type f -iname *.ini -exec sed -inr "s/^MemoryMargin=.*/MemoryMargin=$HALFMEMORY/g" {} \;
-zenity --info --text "MemoryMargin impostato a $HALFMEMORY MB"
+zenity --width=600 --height=400 --info --text "MemoryMargin impostato a $HALFMEMORY MB"
 
 exit
