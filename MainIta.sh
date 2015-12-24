5c0d1d3b4a3d2db34b4cbe01959d2f24026deccd
Kryuko <gian95torino@hotmail.it>
2015-12-24 18:27:05 +0100

    Update MainIta.sh

--- MainIta.sh
+++ MainIta.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 MEM=$(grep -o "Memory:\ [0-9]*" /var/log/Xorg.0.log | grep -Eo "[0-9]+")
-#MEMORY=$(grep -o ".*Memory: .*" /var/log/Xorg.0.log | cut -d \  -f 9)
+
 let MEM=$MEM/1024
 echo $MEM
 
-zenity --width=600 --height=400 --info --text "PREMENDO OK ACCETTI LA NOSTRA LICENZA \n 
+zenity --width=600 --height=400 --info \
+       --title="License" \
+	--text "PREMENDO OK ACCETTI LA NOSTRA LICENZA \n 
 Developed by:\n Gianmaria Generoso, admin of www.italiaunix.com.\n Alberto Pau - DebInst creator https://github.com/TheGatorade/debinst \n
 GNU GPL v3 License - Available at \n https://raw.githubusercontent.com/Kryuko/ue3_linux_opti/master/LICENSE"
-       --title="License" \
-	#--text="Inserisci la tua memoria video in MB" \
-       #--checkbox="I read and accept the terms."
 
 let MEMORY=$MEM-128
 
